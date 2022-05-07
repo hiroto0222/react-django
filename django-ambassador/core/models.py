a772e0c5c4597f71e0e85661d6e46fabe716576a
hiroto0222 <aoyamahiroto0222@gmail.com>
2022-05-07 15:47:55 +0900

    create user models

---
+++ django-ambassador/core/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("User must have an Email!")
+        if not password:
+            raise ValueError("User must have a Password!")
+        
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.is_admin = False
+        user.is_staff = False
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None):
+        if not email:
+            raise ValueError("User must have an Email!")
+        if not password:
+            raise ValueError("User must have a Password!")
+        
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    is_ambassador = models.BooleanField(default=True)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
